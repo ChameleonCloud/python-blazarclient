c2094fa301bfff23837c66f11b91578db84702dd
Jason Anderson <jasonanderson@uchicago.edu>
2020-01-28 10:21:26 -0600

    Don't fetch entire list when looking up by ID
    Blazar APIs support directly fetching a resource (whether host, floating
    ip, or lease) with its ID. However, the default logic for this includes
    a block that supports additionally looking up the resource by name (to
    allow looking up e.g., a lease by it's name.) This requires fetching the
    entire list of all resources and iterating over them until a match is
    found, which is very inefficient. Moreover, this branch would be taken
    even if the input was already a UUID.
    
    This commit changes that behavior so that if a UUID is provided, it is
    used to directly fetch the resource, which seems to be what the original
    intent was in the first place.
    
    Change-Id: I5b1ccf4abfe4aa9068bc011764cac4b3507ef697

--- blazarclient/tests/v1/shell_commands/test_leases.py
+++ blazarclient/tests/v1/shell_commands/test_leases.py
@@ -304,3 +304,81 @@ class UpdateLeaseTestCase(tests.TestCase):
         }
 
         self.assertDictEqual(self.cl.args2body(args), expected)
+
+
+class ShowLeaseTestCase(tests.TestCase):
+
+    def create_show_command(self):
+        mock_lease_manager = mock.Mock()
+        mock_client = mock.Mock()
+        mock_client.lease = mock_lease_manager
+
+        blazar_shell = shell.BlazarShell()
+        blazar_shell.client = mock_client
+        return (leases.ShowLease(blazar_shell, mock.Mock()),
+                mock_lease_manager)
+
+    def test_show_lease(self):
+        show_lease, lease_manager = self.create_show_command()
+        lease_manager.get.return_value = {'id': FIRST_LEASE}
+        mock.seal(lease_manager)
+
+        args = argparse.Namespace(id=FIRST_LEASE)
+        expected = [('id',), (FIRST_LEASE,)]
+
+        self.assertEqual(show_lease.get_data(args), expected)
+        lease_manager.get.assert_called_once_with(FIRST_LEASE)
+
+    def test_show_lease_by_name(self):
+        show_lease, lease_manager = self.create_show_command()
+        lease_manager.list.return_value = [
+            {'id': FIRST_LEASE, 'name': 'first-lease'},
+            {'id': SECOND_LEASE, 'name': 'second-lease'},
+        ]
+        lease_manager.get.return_value = {'id': SECOND_LEASE}
+        mock.seal(lease_manager)
+
+        args = argparse.Namespace(id='second-lease')
+        expected = [('id',), (SECOND_LEASE,)]
+
+        self.assertEqual(show_lease.get_data(args), expected)
+        lease_manager.list.assert_called_once_with()
+        lease_manager.get.assert_called_once_with(SECOND_LEASE)
+
+
+class DeleteLeaseTestCase(tests.TestCase):
+
+    def create_delete_command(self):
+        mock_lease_manager = mock.Mock()
+        mock_client = mock.Mock()
+        mock_client.lease = mock_lease_manager
+
+        blazar_shell = shell.BlazarShell()
+        blazar_shell.client = mock_client
+        return (leases.DeleteLease(blazar_shell, mock.Mock()),
+                mock_lease_manager)
+
+    def test_delete_lease(self):
+        delete_lease, lease_manager = self.create_delete_command()
+        lease_manager.delete.return_value = None
+        mock.seal(lease_manager)
+
+        args = argparse.Namespace(id=FIRST_LEASE)
+        delete_lease.run(args)
+
+        lease_manager.delete.assert_called_once_with(FIRST_LEASE)
+
+    def test_delete_lease_by_name(self):
+        delete_lease, lease_manager = self.create_delete_command()
+        lease_manager.list.return_value = [
+            {'id': FIRST_LEASE, 'name': 'first-lease'},
+            {'id': SECOND_LEASE, 'name': 'second-lease'},
+        ]
+        lease_manager.delete.return_value = None
+        mock.seal(lease_manager)
+
+        args = argparse.Namespace(id='second-lease')
+        delete_lease.run(args)
+
+        lease_manager.list.assert_called_once_with()
+        lease_manager.delete.assert_called_once_with(SECOND_LEASE)
