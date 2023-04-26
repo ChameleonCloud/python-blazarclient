c2e02698451462283139a904992e1f0a615c9419
Anish Reddy <2anishreddy@gmail.com>
2023-04-26 10:48:02 -0500

    Fix network-list not showing the column when all of them not have it (#23)
    * Fix network-list not showing the column when all of them not have it
    
    * Display empty string if resource does not have the property

--- blazarclient/utils.py
+++ blazarclient/utils.py
@@ -93,8 +93,8 @@ def get_item_properties(item, fields, mixed_case_fields=None, formatters=None):
             else:
                 field_name = field.lower().replace(' ', '_')
             if not hasattr(item, field_name) and isinstance(item, dict):
-                # display null if the resource does not have a field
-                data = item.get(field_name, "null")
+                # display empty string if the resource does not have a field
+                data = item.get(field_name, '')
             else:
                 data = getattr(item, field_name, '')
             if data is None:
